dd041fe70e00fbadb2c25a3fb6f609f9565093a8
Ilya Omelyukhin <i.omelyuhin@gmail.com>
2020-03-29 08:50:17 +0300

    first commit

---
+++ gulp/tasks/svgSprite.js
@@ -0,0 +1,15 @@
+const gulp = require("gulp");
+const svgstore = require("gulp-svgstore");
+const rename = require("gulp-rename");
+
+module.exports = function svgSprite() {
+  return gulp
+    .src("src/assets/img/icons/*.svg")
+    .pipe(
+      svgstore({
+        inlineSvg: true
+      })
+    )
+    .pipe(rename("sprite.svg"))
+    .pipe(gulp.dest("build/assets/img"));
+};
